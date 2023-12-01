7f3dcaa1a23347061e9dc7e8e1451540ada7149f
munjeli <1256674+munjeli@users.noreply.github.com>
2023-12-01 05:35:55 -0600

    fix: lands on signin page instead of blank home

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { shallow, screen } from "@testing-library/react";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import App from "./App";
 
-test("renders without crashing", () => {
-  shallow(<App />);
-  const linkElement = screen.get(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Routing integration tests", () => {
+  test("navigating to /signup", async () => {
+    // Given a browser history object
+    const history = createMemoryHistory();
+
+    // And the app is rendered with the history object
+    render(
+      <Router history={history}>
+        <App/>
+      </Router>
+    );
+
+    // When the "/signup" link is clicked
+    fireEvent.click(screen.getByText(/No account/));
+
+    // Then the URL should update to "/signup"
+    await waitFor(() => expect(history.location.pathname).toEqual("/signup"));
+
+    // And the about page should be displayed
+    // expect(screen.getByText(/Sign Up/i)).toBeTruthy();
+  });
+})
+
+//  test("navigating to non-existing route", async () => {
+// Given a browser history…
+
