3efa8a69baf64cfe34f739d598f02f8ae968251a
munjeli <1256674+munjeli@users.noreply.github.com>
2023-12-04 09:19:51 -0600

    fix: signin test running

---
+++ src/components/SignIn.test.js
@@ -0,0 +1,27 @@
+import React from "react"
+import {render, screen} from "@testing-library/react"
+import fireEvent from "@testing-library/user-event"
+import "@testing-library/jest-dom"
+import SignIn from "./SignIn"
+import { BrowserRouter as Router } from "react-router-dom";
+import {act} from "react-dom/test-utils";
+
+
+test("loads and displays SignIn page", async () => {
+  // ARRANGE
+  render(
+    <Router>
+      <SignIn url="/signin" />
+    </Router>)
+
+  // ACT
+  act(() => {
+    fireEvent.click(screen.getByText("Sign in"))
+    screen.findByRole("heading")
+  }
+  )
+
+
+  // ASSERT
+  expect(screen.getByRole("heading")).toHaveTextContent("Sign in")
+})
