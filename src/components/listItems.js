1b43308e9ea55c1c93ef8c656ef039246e80af72
munjeli <1256674+munjeli@users.noreply.github.com>
2023-12-04 11:10:33 -0600

    feat: added links to listItemButtons in drawer nav

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -12,37 +12,37 @@ import {TunnelbotCurtainIcon,
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton component={ Link } to={ "/"}>
+    <ListItemButton component={ Link } to={ "/curtains" }>
       <ListItemIcon>
         <TunnelbotCurtainIcon />
       </ListItemIcon>
       <ListItemText primary="Curtains" />
     </ListItemButton>
-    <ListItemButton component={ Link } to={ "/"}>
+    <ListItemButton component={ Link } to={ "/heating" }>
       <ListItemIcon>
         <TunnelbotHeatingIcon />
       </ListItemIcon>
       <ListItemText primary="Heating" />
     </ListItemButton>
-    <ListItemButton component={ Link } to={ "/"}>
+    <ListItemButton component={ Link } to={ "/irrigation" }>
       <ListItemIcon>
         <TunnelbotIrrigationIcon />
       </ListItemIcon>
       <ListItemText primary="Irrigation" />
     </ListItemButton>
-    <ListItemButton component={ Link } to={ "/"}>
+    <ListItemButton component={ Link } to={ "/camera" }>
       <ListItemIcon>
         <TunnelbotCameraIcon />
       </ListItemIcon>
       <ListItemText primary="Camera" />
     </ListItemButton>
-    <ListItemButton component={ Link } to={ "/"}>
+    <ListItemButton component={ Link } to={ "/lights" }>
       <ListItemIcon>
         <TunnelbotLightsIcon />
       </ListItemIcon>
       <ListItemText primary="Lights" />
     </ListItemButton>
-    <ListItemButton component={ Link } to={ "/"}>
+    <ListItemButton component={ Link } to={ "/weather" }>
       <ListItemIcon>
         <TunnelbotWeatherIcon />
       </ListItemIcon>
