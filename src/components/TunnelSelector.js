7f92bc1e45bcd9d6a9cdbfb91c8566e3c3a75008
munjeli <1256674+munjeli@users.noreply.github.com>
2023-12-11 06:54:56 -0600

    feat: added select to tunnelbot so one can choose which tunnel data to display

---
+++ src/components/TunnelSelector.js
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import FilledInput from "@mui/material/FilledInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "tunnel-1",
+  "tunnel-2",
+  "tunnel-3",
+];
+
+function getStyles(name, tunnelName, theme) {
+  return {
+    fontWeight:
+      tunnelName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function TunnelSelector() {
+  const theme = useTheme();
+  const [tunnelName, setTunnelName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTunnelName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value,
+    );
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ mb: 1, width: "100%" }}>
+        <InputLabel id="tunnel-selector-label">Tunnels</InputLabel>
+        <Select
+          labelId="tunnel-selector-label"
+          id="tunnel-selector"
+          multiple
+          value={tunnelName}
+          onChange={handleChange}
+          input={<FilledInput label="Tunnels" />}
+          MenuProps={MenuProps}
+        >
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, tunnelName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
